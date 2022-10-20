820951bdccb732607922b7eedd165946656128ed
Roner <Roner@hotmail.se>
2022-10-21 01:39:02 +0200

    Results container added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import { CssBaseline, Paper, styled, ThemeProvider } from "@mui/material";
+import { CssBaseline, Grid, Paper, styled, ThemeProvider } from "@mui/material";
 import { Container } from "@mui/system";
 import theme from "./theme/theme";
+
+const OutputContainer = styled("div")(({ theme }) => ({
+  width: "100%",
+  textAlign: "right",
+  height: "2em",
+  padding: theme.spacing(2),
+  fontSize: "3em",
+  overflow: "hidden",
+}));
 
 const CalculatorBase = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(2),
@@ -13,7 +22,13 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth="sm">
-        <CalculatorBase elevation={3}>Hello World</CalculatorBase>
+        <CalculatorBase elevation={3}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <OutputContainer>0</OutputContainer>
+            </Grid>
+          </Grid>
+        </CalculatorBase>
       </Container>
     </ThemeProvider>
   );
